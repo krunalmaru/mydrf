3f43a279ceb87f8fe9a01f7bdca4c0ac2cdb27f6
krunal <krunal.maru1106@gmail.com>
2023-01-07 15:51:46 +0530

    create model and model serializer

--- classbaseddrf/apps/models.py
+++ classbaseddrf/apps/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from rest_framework import serializers
 
 # Create your models here.
+class Course(models.Model):
+    name= models.CharField(max_length=70)
+    price = models.IntegerField()
+    discount = models.IntegerField()
+    duration = models.DurationField()
+    authorname = models.CharField(max_length=100)
+    
+    def __str__(self) -> str:
+        return self.name
+
+class CourseModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = "__all__"
--- classbaseddrf/apps/views.py
+++ classbaseddrf/apps/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Course,CourseModelSerializer
 # Create your views here.
+
+class CourseListView(APIView):
+    def get(self, request):
+        cou = Course.objects.all()
+        serializer = CourseModelSerializer(cou, many=True)
+        return Response(serializer.data)

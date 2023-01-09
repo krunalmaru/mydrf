3ab4640b31b8c1e179c934fcf17f1647d1a9c999
krunal <krunal.maru1106@gmail.com>
2023-01-09 13:03:53 +0530

    ListAPIView to get all data and CreateAPIView to create new data

--- classbaseddrf/apps/views.py
+++ classbaseddrf/apps/views.py
@@ -4,49 +4,87 @@ from rest_framework.response import Response
 from .models import Course,CourseModelSerializer
 from django.http import Http404
 from rest_framework import status
+from rest_framework import mixins, generics
 # Create your views here.
 
-class CourseListView(APIView):
+
+'''########## listApiview = listmodelmixin + genericApiView ###########'''
+'''########## generics = get ,post,put,delete ############'''
+
+
+class CourseListView(generics.ListAPIView, generics.CreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseModelSerializer
+
+
+'''
+class CourseListView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseModelSerializer
+
+
     def get(self, request):
-        cou = Course.objects.all()
-        serializer = CourseModelSerializer(cou, many=True)
-        return Response(serializer.data)
+        return self.list(request)
     
     def post(self, request):
-        serializer = CourseModelSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors)
+        return self.create(request)
+'''
 
-class CorseDetailView(APIView):
-    def get_course(self,pk):
-        try:
-           return Course.objects.get(pk=pk)
-        except Course.DoesNotExist:
-            raise Http404
+class CorseDetailView(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.DestroyModelMixin, mixins.UpdateModelMixin):
+    queryset = Course.objects.all()
+    serializer_class = CourseModelSerializer
 
     def get(self,request,pk):
-        course = self.get_course(pk)
-        serializer = CourseModelSerializer(course)
-        return Response(serializer.data)
-    def post(self,request,pk):
-        serialiser = CourseModelSerializer(data=request.data)
-        if serialiser.is_valid():
-            serialiser.save()
-            return Response(serialiser.data)
-        return Response(serialiser.errors)
+        return self.retrieve(request,pk)
 
-    def delete(self,request,pk):
-        course = self.get_course(pk)
-        course.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request,pk):
+        return self.destroy(request,pk)
+
+    def put(self, request,pk):
+        return self.update(request,pk)
+
+
+# class CourseListView(APIView):
+#     def get(self, request):
+#         cou = Course.objects.all()
+#         serializer = CourseModelSerializer(cou, many=True)
+#         return Response(serializer.data)
+    
+#     def post(self, request):
+#         serializer = CourseModelSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors)
+
+# class CorseDetailView(APIView):
+#     def get_course(self,pk):
+#         try:
+#            return Course.objects.get(pk=pk)
+#         except Course.DoesNotExist:
+#             raise Http404
+
+#     def get(self,request,pk):
+#         course = self.get_course(pk)
+#         serializer = CourseModelSerializer(course)
+#         return Response(serializer.data)
+#     # def post(self,request,pk):
+#     #     serialiser = CourseModelSerializer(data=request.data)
+#     #     if serialiser.is_valid():
+#     #         serialiser.save()
+#     #         return Response(serialiser.data)
+#     #     return Response(serialiser.errors)
+
+#     def delete(self,request,pk):
+#         course = self.get_course(pk)
+#         course.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
        
-    def put(self,request,pk):
-        course = self.get_course(pk)
-        serializer = CourseModelSerializer(course, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+#     def put(self,request,pk):
+#         course = self.get_course(pk)
+#         serializer = CourseModelSerializer(course, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors)
         
